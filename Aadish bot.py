9331c3e051c41531a4518b3c8621a3d7a921e0e4
AadishY <101470733+AadishY@users.noreply.github.com>
2024-05-05 20:32:58 +0530

    Update Aadish bot.py

--- Aadish bot.py
+++ Aadish bot.py
@@ -1,14 +1,10 @@
 from groq import Groq
 
-api_key = "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"
-client = Groq(api_key=api_key)
+client = Groq(api_key="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")
 
 while True:
     user_input = input("You: ")
     
-    if user_input.lower() == "exit":
-        print("Aadish: Goodbye!")
-        break
     
     bot_response = client.chat.completions.create(
         messages=[{"role": "user", "content": user_input}],
@@ -18,3 +14,4 @@ while True:
     ).choices[0].message.content
 
     print("\nAadish:", bot_response, "\n")
+
