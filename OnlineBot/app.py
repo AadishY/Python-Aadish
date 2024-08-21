5ac5eccc9bfebd4429058006cb322bfc3f03cc87
Aadish <101470733+AadishY@users.noreply.github.com>
2024-08-21 18:38:23 +0530

    Update app.py

--- OnlineBot/app.py
+++ OnlineBot/app.py
@@ -16,7 +16,6 @@ BACKGROUND_IMAGE_URL = "https://cdn.jsdelivr.net/gh/AadishY/Python-Aadish@main/m
 hide_st_style = """
             <style>
             #MainMenu {visibility: hidden;}
-            .viewerBadge_link__qRIco, .viewerBadge_container__r5tak, .styles_viewerBadge__CvC9N {display: none;}
             footer {visibility: hidden;}
             header {visibility: hidden;}
             </style>
